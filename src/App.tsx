784b5ce11528cf9694ec4b75127a3250ecf672a9
Jimmy Bustillo <jimmy.bustillo@outlook.com>
2024-06-06 21:25:06 -0600

    Itera los items para mostrarlos en pantalla

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,7 @@
+import MenuItem from "./components/MenuItem"
 import { menuItems } from "./data/db"
 
 function App() {
-  console.log(menuItems)
-
   return (
     <>
       <header className='bg-teal-400 py-5'>
@@ -10,6 +9,20 @@ function App() {
           Calculadora de propinas y Consumo
         </h1>
       </header>
+
+      <main className='max-w-7xl mx-auto py-20 grid md:grid-cols-2'>
+        <div className="p-5">
+          <h2 className=" text-4xl font-black">Menu</h2>
+          <div className="space-y-3 mt-10">
+            {menuItems.map((item) => (
+              <MenuItem key={item.id} item={item} />
+            ))}
+          </div>
+        </div>
+        <div>
+          <h2>Consumo</h2>
+        </div>
+      </main>
     </>
   )
 }
