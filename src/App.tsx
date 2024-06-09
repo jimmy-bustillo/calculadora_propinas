ad3536e4f958402663f8c6ef97f4c744fcaa4306
Jimmy Bustillo <jimmy.bustillo@outlook.com>
2024-06-08 21:26:46 -0600

    Utiliza el customHook para agregar items usando el button

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
 import MenuItem from "./components/MenuItem"
 import { menuItems } from "./data/db"
+import useOrder from "./hooks/useOrder"
 
 function App() {
+  const { addItem } = useOrder()
+
   return (
     <>
       <header className='bg-teal-400 py-5'>
@@ -11,11 +14,11 @@ function App() {
       </header>
 
       <main className='max-w-7xl mx-auto py-20 grid md:grid-cols-2'>
-        <div className="p-5">
-          <h2 className=" text-4xl font-black">Menu</h2>
-          <div className="space-y-3 mt-10">
+        <div className='p-5'>
+          <h2 className=' text-4xl font-black'>Menu</h2>
+          <div className='space-y-3 mt-10'>
             {menuItems.map((item) => (
-              <MenuItem key={item.id} item={item} />
+              <MenuItem key={item.id} item={item} addItem={addItem} />
             ))}
           </div>
         </div>
