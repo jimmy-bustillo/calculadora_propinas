d729a902aedd292bf14310d912eb8ac96c412cab
Jimmy Bustillo <jimmy.bustillo@outlook.com>
2024-06-22 21:30:32 -0600

    Agrega el type de button

--- src/components/MenuItem.tsx
+++ src/components/MenuItem.tsx
@@ -1,18 +1,19 @@
-import type { MenuItem } from "../types"
+import type { MenuItem } from "../types";
 
 type MenuItemProps = {
-  item: MenuItem
-  addItem: (item: MenuItem) => void
-}
+	item: MenuItem;
+	addItem: (item: MenuItem) => void;
+};
 
 export default function MenuItem({ item, addItem }: MenuItemProps) {
-  return (
-    <button
-      className=' border-2 border-teal-400 hover:bg-teal-200 w-full p-3 flex justify-between'
-      onClick={() => addItem(item)}
-    >
-      <p>{item.name}</p>
-      <p className=' font-black'>${item.price}</p>
-    </button>
-  )
+	return (
+		<button
+			type="button"
+			className=" border-2 border-teal-400 hover:bg-teal-200 w-full p-3 flex justify-between"
+			onClick={() => addItem(item)}
+		>
+			<p>{item.name}</p>
+			<p className=" font-black">${item.price}</p>
+		</button>
+	);
 }
