45895d3d1ba2a5265e1769495c6297e60816b971
Jimmy Bustillo <jimmy.bustillo@outlook.com>
2024-06-22 21:30:14 -0600

    Añade el prop para resetear la orden

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import { menuItems } from "./data/db";
 import useOrder from "./hooks/useOrder";
 
 function App() {
-	const { order, tip, setTip, addItem, removeItem } = useOrder();
+	const { order, tip, setTip, addItem, removeItem, placeOrder } = useOrder();
 
 	return (
 		<>
@@ -26,9 +26,15 @@ function App() {
 					</div>
 				</div>
 				<div className=" border border-dashed border-slate-300 p-5 rounded-lg space-y-10">
-					<OrderContents order={order} removeItem={removeItem} />
-					<TipPercentageForm setTip={setTip} />
-					<OrderTotals order={order} tip={tip} />
+					{order.length > 0 ? (
+						<>
+							<OrderContents order={order} removeItem={removeItem} />
+							<TipPercentageForm setTip={setTip} tip={tip} />
+							<OrderTotals order={order} tip={tip} placeOrder={placeOrder} />
+						</>
+					) : (
+						<p className=" text-center">La Orden esta vacia</p>
+					)}
 				</div>
 			</main>
 		</>
